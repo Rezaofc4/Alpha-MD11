78b59993a49c264ae067d8123d2f1d8ec7aab381
Rezaofc4 <106759741+Rezaofc4@users.noreply.github.com>
2022-06-05 18:37:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "NitsuBotz-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ JakaOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://instagram.com/itzme_nitsu" //bebas asal jan hapus
-global.youtube = "https://youtube.com/channel/UC7jyjV6kcG6qTV_VcJtJzzA" //bebas asal jan hapus
-global.github = "https/github.com/Jaka09b/" //bebas
-global.email = "jkasmprn@gmail.com" //bebas
+global.botname = "EzaBotz-Mdོ" //namabot kalian
+global.ownername= "© Reza Ganteng ×፝֟͜×" //nama kalian
+global.myweb ="https://chat.whatsapp.com/BCgUSCxQltq8SwcpJe1tDf" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UCoUIE95oCiyo_Ps0l9rhcdw" //bebas asal jan hapus
+global.github = "https://github.com/Rezaofc" //bebas
+global.email = "nb.boruto.official@gmail.com" //bebas
 global.region = "Japanese" //bebas
-global.ownernomer = "6285785694474" // nomor wa kalian
-global.ownernomerr = "+6285785694474" //nmr wa kalian
+global.ownernomer = "+6289606790112" // nomor wa kalian
+global.ownernomerr = "+6289606790112" //nmr wa kalian
 global.thumbnail = "./image/lolh.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasip.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6285785694474","6285785694474","60107155057"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Nitsu-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh JakaOfc' //sticker wm ganti nama kalian
+global.owner = ["6289606790112","6281352905695","6281515696780"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© EzaBot-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh Reza Ganteng ' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
